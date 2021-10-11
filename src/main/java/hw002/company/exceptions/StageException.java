73defcf36bb1a9ed2fbd35e45a67f66ebd04789e
Ruslan <rus-sto@tut.by>
2021-10-11 15:48:40 +0300

    HW002-HW005 Logger and first exception

--- src/main/java/hw002/company/exceptions/StageException.java
+++ src/main/java/hw002/company/exceptions/StageException.java
@@ -1,9 +1,15 @@
 package hw002.company.exceptions;
 
+/**
+ * Масиив, циклы, больше абстракций, см замечания., Много разных коснат, разные стены и др
+ */
+
 public class StageException extends Exception{
 
 
     public StageException(String s) {
+
+
 
     }
 }
